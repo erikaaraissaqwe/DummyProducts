99680e7ea7a97d40239c439d7df4c2dcb0e157fa
Erika Raissa Bueno da Silva <erikaaraissaqwe@gmail.com>
2024-02-13 23:21:13 -0300

    Aula 3

--- app/src/main/java/br/edu/ifsp/scl/sdm/dummyproducts/ui/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/dummyproducts/ui/MainActivity.kt
@@ -1,14 +1,21 @@
 package br.edu.ifsp.scl.sdm.dummyproducts.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import br.edu.ifsp.scl.sdm.dummyproducts.R
 import br.edu.ifsp.scl.sdm.dummyproducts.adapter.ProductAdapter
+import br.edu.ifsp.scl.sdm.dummyproducts.adapter.ProductImageAdapter
 import br.edu.ifsp.scl.sdm.dummyproducts.databinding.ActivityMainBinding
 import br.edu.ifsp.scl.sdm.dummyproducts.model.Product
 import br.edu.ifsp.scl.sdm.dummyproducts.model.ProductsList
 import com.google.gson.Gson
+import java.io.BufferedInputStream
 import java.io.IOException
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
@@ -26,6 +33,12 @@ class MainActivity : AppCompatActivity() {
         ProductAdapter(this, productList)
     }
 
+    private val productImageList: MutableList<Bitmap> = mutableListOf()
+
+    private val productImageAdapter: ProductImageAdapter by lazy {
+        ProductImageAdapter(this, productImageList)
+    }
+
     companion object{
         const val PRODUCTS_ENDPOINT = "https://dummyjson.com/products/"
     }
@@ -38,10 +51,72 @@ class MainActivity : AppCompatActivity() {
             title = getString(R.string.app_name)
         })
 
-        amb.productsSp.adapter = productAdapter
+        amb.productsSp.apply {
+            adapter = productAdapter
+            onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                   val size = productImageList.size
+                    productImageList.clear()
+                    productImageAdapter.notifyItemRangeRemoved(0, size)
+                    retrieveProductsImages(productList[position])
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                  // Nothing
+                }
+
+            }
+        }
+
+        amb.productImagesRv.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = productImageAdapter
+        }
 
         retrieveProducts()
 
+    }
+
+    private fun retrieveProductsImages(product: Product) {
+        Thread {
+            product.images.forEach { imageUrl ->
+                val imageConnection = URL(imageUrl)
+                    .openConnection() as HttpURLConnection
+
+                try {
+                    if (imageConnection.responseCode == HTTP_OK) {
+
+                        BufferedInputStream(imageConnection.inputStream).let {
+                            val imageBitmap = BitmapFactory.decodeStream(it)
+
+                            runOnUiThread {
+                                productImageList.add(imageBitmap)
+                                productImageAdapter.notifyItemInserted(productImageList.lastIndex)
+                            }
+                        }
+                    } else {
+                        runOnUiThread {
+                            Toast.makeText(
+                                this,
+                                getString(R.string.request_problem),
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                } catch (e: IOException) {
+                    runOnUiThread { Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show() }
+                } catch (e: Exception) {
+                    runOnUiThread { Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show() }
+                } finally {
+                    imageConnection.disconnect()
+                }
+            }
+        }.start()
     }
 
     private fun retrieveProducts(){
